359dcdbf0a0623233eedeb6fba2dd3d9ca5cbbfa
LUIS HENRIQUE SEGUNDO <002332@cesed.local>
2021-07-27 12:22:02 -0300

    Refatorando operacao de post people

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -1,11 +1,13 @@
 package one.digitalinovation.personapi.controller;
 
-import one.digitalinovation.personapi.dto.MessageResponseDTO;
-import one.digitalinovation.personapi.entity.Person;
+import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinovation.personapi.dto.request.PersonDTO;
 import one.digitalinovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -18,12 +20,11 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
-
-
-
 }
