fdc6af3883a59500f7362fbe73f6e610f60e7ed3
LUIS HENRIQUE SEGUNDO <002332@CESED.LOCAL>
2021-07-26 22:41:29 -0300

    development person create operation

---
+++ src/main/java/one/digitalinovation/personapi/enums/PhoneType.java
@@ -0,0 +1,16 @@
+package one.digitalinovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    NOME("Nome"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+
+}
